9542dac44f9450892ae941383db372eb67d01b85
Karans11 <karansarvaiya11@gmail.com>
2026-04-24 08:34:05 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Vulnerable demo app for Wiz CLI scanning"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)  # debug=True is also a finding
