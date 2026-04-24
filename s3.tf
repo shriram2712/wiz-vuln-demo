f66d7957736f4fb5213d2635197a6b1050394054
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2026-04-24 11:07:25 +0530

    Add S3 bucket and related resources

--- s3.tf
+++ s3.tf
@@ -1,4 +1,14 @@
-# VULN: Public access block disabled
+resource "aws_s3_bucket" "vulnerable" {
+  bucket = "vulnerable-app-data-${random_string.suffix.result}"
+
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 resource "aws_s3_bucket_public_access_block" "vulnerable" {
   bucket = aws_s3_bucket.vulnerable.id
 
@@ -6,4 +16,12 @@ resource "aws_s3_bucket_public_access_block" "vulnerable" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_versioning" "vulnerable" {
+  bucket = aws_s3_bucket.vulnerable.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
 }
